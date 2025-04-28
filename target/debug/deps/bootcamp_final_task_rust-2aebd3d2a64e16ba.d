34af551c80f02adcdead0ce7202c939038cf4607
paradogs-7 <vnyz2002@gmail.com>
2025-04-28 11:47:32 +0300

    First commit

---
+++ target/debug/deps/bootcamp_final_task_rust-2aebd3d2a64e16ba.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-final-task-rust/target/debug/deps/libbootcamp_final_task_rust-2aebd3d2a64e16ba.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-final-task-rust/target/debug/deps/bootcamp_final_task_rust-2aebd3d2a64e16ba.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bootcamp_final_task_rust-e9ce84f80e34c5d3.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-final-task-rust/target/debug/deps/libbootcamp_final_task_rust-e9ce84f80e34c5d3.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-final-task-rust/target/debug/deps/bootcamp_final_task_rust-e9ce84f80e34c5d3.d: src/main.rs
+
+src/main.rs:
